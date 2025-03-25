b376deca034902217460b4fba75a66de4abc28e3
vannam195 <namlv1252@ut.edu.vn>
2025-03-25 22:24:46 +0700

    controller

---
+++ AuthController.java
@@ -0,0 +1,23 @@
+package com.skincare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register";
+    }
+
+    @GetMapping("/forgot-password")
+    public String showForgotPasswordPage() {
+        return "forgot-password";
+    }
+} 
